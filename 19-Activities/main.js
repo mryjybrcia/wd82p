925332bb8ac0402e53f80b129f6104b0cba75b82
Mary Joy Labadan <mryjybrcia@gmail.com>
2023-09-01 22:01:24 +0800

    Update index.html

---
+++ 19-Activities/main.js
@@ -0,0 +1,62 @@
+console.log("Hello, Async Activity!");
+alert("Activity - September 1, 2023");
+
+// EXERCISE 1 //
+// Declare a variable called 'balance' and assign it a starting balance of 1000.
+// Create variables 'transaction1', 'transaction2', and 'transaction3' to represent three transactions.
+// Update the 'balance' variable after each transaction:
+// - 'transaction1': Add 500 to 'balance'.
+// - 'transaction2': Deduct 200 from 'balance'.
+// - 'transaction3': Deduct 1000 from 'balance'.
+
+// Your code here
+let balance = 1000;
+
+let transaction1 = 500;
+let transaction2 = 200;
+let transaction3 = 1000;
+
+console.log(balance + transaction1);
+console.log(balance - transaction2);
+console.log(balance - transaction3);
+
+// Log the final 'balance' after all three transactions.
+let finalBalance = balance + transaction1 - transaction2 - transaction3;
+console.log(finalBalance);
+
+// EXERCISE 2 //
+// Create three variables 'testScore1', 'testScore2', and 'testScore3' and assign them different test scores as numbers.
+// Calculate the average test score of these three scores and store it in a variable called 'averageScore'.
+// Round the 'averageScore' to the nearest integer using Math.round().
+
+// Your code here
+// let testScore1 = 78;
+// let testScore2 = 84;
+// let testScore3 = 92;
+// let averageScore = testScore1 + testScore2 + testScore3 / 3;
+
+const testScore1 = 78;
+const testScore2 = 84;
+const testScore3 = 92;
+const averageScore = Math.round((testScore1 + testScore2 + testScore3) / 3);
+// Log the 'averageScore' to the console.
+console.log(averageScore); //decimal result is 84.66666666666667
+
+// EXERCISE 3 //
+// Create a variable called 'temperature' and assign it a temperature value in Celsius.
+// Write a conditional statement (if-else) to determine the weather condition based on the temperature:
+// - If the temperature is above 30°C, log "It's hot outside!"
+// - If the temperature is between 20°C and 30°C (inclusive), log "The weather is pleasant."
+// - If the temperature is below 20°C, log "It's a bit chilly."
+
+// Your code here
+const tempInCelsius = parseInt(prompt("How hot is it outside today?"));
+// console.log(typeof tempInCelsius);
+
+if (tempInCelsius > 30) {
+  console.log("It's hot outside!");
+} else if (tempInCelsius >= 20) {
+  console.log("The weather is pleasant.");
+} else {
+  console.log("It's a bit chilly.");
+}
