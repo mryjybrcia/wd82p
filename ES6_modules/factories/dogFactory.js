6c122962dc31a76c833f9dad6d5a00c63febb039
Mary Joy Labadan <141330072+mryjybrcia@users.noreply.github.com>
2023-09-27 19:14:12 +0800

    Add updated ES6 modules

--- ES6_modules/factories/dogFactory.js
+++
@@ -1,3 +0,0 @@
-export function dogFactory(name, breed) {
-  return { name, breed };
-}
--- ES6_modules/main.js
+++ ES6_modules/main.js
@@ -1,12 +1,24 @@
-// ES6 Modules
-import "./style.css";
-import { dogFactory } from "./factories/dogFactory.js";
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-const dog1 = dogFactory("Fido", "Mixed");
-console.log(dog1);
-
-document.querySelector("#app").innerHTML = `
+document.querySelector('#app').innerHTML = `
   <div>
-   <hi>Hello, Vite!</hi>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
   </div>
-`;
+`
+
+setupCounter(document.querySelector('#counter'))
